f7b57adadbab9227aa36016900b5e8882aad1d7b
CyberROFL <i.nizametdinov@gmail.com>
2013-09-12 11:04:31 +0400

    Added list control and two buttons

--- HelloYandex/resource/resource.h
+++ HelloYandex/resource/resource.h
@@ -4,6 +4,9 @@
 //
 #define IDI_MAIN                        100
 #define IDD_MAIN                        100
+#define IDC_LIST                        1000
+#define IDC_ADD                         1001
+#define IDC_DELETE                      1002
 
 // Next default values for new objects
 // 
@@ -11,7 +14,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        100
 #define _APS_NEXT_COMMAND_VALUE         40000
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           100
 #endif
 #endif
